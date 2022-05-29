1a2916de37ae5b11ccd7d1c446b997a3f1d5269d
yidaoJ <jcxsusan@gmail.com>
2022-05-29 20:42:12 +0200

    removed sensitive content

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -43,18 +43,6 @@ export const ProjectsPage: React.VFC = () => {
           image='https://s2.loli.net/2022/04/19/BzroIbfMD4nYSwC.png'
           links={LinksReactMovingText}
         />
-        <ProjectPreview
-          title='static websites'
-          description='static websites are built with react and typescript, whose contents store in headless CMS strapi. Communication between front-end and back-end is via REST-Api realised.'
-          image='https://s2.loli.net/2022/05/26/dSDaP4kAGoITtsQ.png'
-          links={LinksWebsites}
-        />
-        <ProjectPreview
-          title='Single-Page-Applications for municipal utility efficiency network '
-          description='static websites are built with react and typescript, whose contents store in headless CMS strapi. Communication between front-end and back-end is via REST-Api realised.'
-          image='https://s2.loli.net/2022/05/28/GHfsUb4qdJ9SwpK.png'
-          links={LinksWebsites}
-        />
       </FlexContainer>
     </PageContainer>
   )
