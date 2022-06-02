b1d26d1f6163d4210a6bde03fb5952611ce4c0ca
yidaoJ <jcxsusan@gmail.com>
2022-06-02 21:41:42 +0200

    add .htaccess

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
-import { Routes, Route, BrowserRouter } from 'react-router-dom'
+import { Routes, Route, HashRouter } from 'react-router-dom'
 import { Home, Contact, About, ProjectsPage } from './pages'
 import { AppTheme, GlobalStyles } from './styles'
 import { Layout } from './components'
@@ -8,7 +8,7 @@ import { Layout } from './components'
 function App() {
   return (
     <ThemeProvider theme={AppTheme}>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path='/' element={<Layout />}>
             <Route index element={<Home />} />
@@ -17,7 +17,7 @@ function App() {
             <Route path='contact' element={<Contact />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <GlobalStyles />
     </ThemeProvider>
   )
